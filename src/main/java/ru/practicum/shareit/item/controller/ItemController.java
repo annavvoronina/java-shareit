031847e29f3efa330702c866501e21e1dddfea70
Anna <annavvoronina@yandex.ru>
2023-04-09 01:50:51 +0300

    Реализация проекта ShareIt (спринт 14)

--- src/main/java/ru/practicum/shareit/item/controller/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/controller/ItemController.java
@@ -3,6 +3,7 @@ package ru.practicum.shareit.item.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.item.dto.CommentDto;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.service.ItemService;
 
@@ -14,7 +15,7 @@ import java.util.List;
 @RequestMapping("/items")
 public class ItemController {
     private final ItemService itemService;
-    private static final String XSHARERUSERID = "X-Sharer-User-Id";
+    private static final String X_SHARER_USER_ID = "X-Sharer-User-Id";
 
     @Autowired
     public ItemController(ItemService itemService) {
@@ -22,23 +23,23 @@ public class ItemController {
     }
 
     @GetMapping
-    public List<ItemDto> findAll(@RequestHeader(XSHARERUSERID) int userId) {
+    public List<ItemDto> findAll(@RequestHeader(X_SHARER_USER_ID) Long userId) {
         return itemService.getItemsByUserId(userId);
     }
 
     @PostMapping
-    public ItemDto createItem(@Valid @RequestBody ItemDto itemDto, @RequestHeader(XSHARERUSERID) int userId) {
+    public ItemDto createItem(@Valid @RequestBody ItemDto itemDto, @RequestHeader(X_SHARER_USER_ID) Long userId) {
         return itemService.createItem(itemDto, userId);
     }
 
     @PatchMapping("/{itemId}")
-    public ItemDto updateItem(@RequestBody ItemDto itemDto, @PathVariable int itemId, @RequestHeader(XSHARERUSERID) int userId) {
+    public ItemDto updateItem(@RequestBody ItemDto itemDto, @PathVariable Long itemId, @RequestHeader(X_SHARER_USER_ID) Long userId) {
         return itemService.updateItem(itemDto, itemId, userId);
     }
 
     @GetMapping("/{itemId}")
-    public ItemDto getItem(@PathVariable int itemId) {
-        return itemService.getItemById(itemId);
+    public ItemDto getItem(@PathVariable Long itemId, @RequestHeader(X_SHARER_USER_ID) Long userId) {
+        return itemService.getItemById(itemId, userId);
     }
 
     @GetMapping("/search")
@@ -47,7 +48,14 @@ public class ItemController {
     }
 
     @DeleteMapping("/{itemId}")
-    public void removeItem(@PathVariable int itemId) {
+    public void removeItem(@PathVariable Long itemId) {
         itemService.removeItemById(itemId);
+    }
+
+    @PostMapping("{itemId}/comment")
+    public CommentDto createComment(@Valid @RequestBody CommentDto commentDto,
+                                    @RequestHeader(X_SHARER_USER_ID) Long userId,
+                                    @PathVariable Long itemId) {
+        return itemService.createComment(commentDto, userId, itemId);
     }
 }
